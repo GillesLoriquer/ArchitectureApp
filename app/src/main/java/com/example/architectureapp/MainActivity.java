0088820ea43196391e5081a4630d4170250e6d40
Gilles Loriquer <gilles.loriquer@gmail.com>
2019-09-21 18:25:34 +0200

    Implémentation du NoteViewModel

--- app/src/main/java/com/example/architectureapp/MainActivity.java
+++ app/src/main/java/com/example/architectureapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.architectureapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.architectureapp.database.Note;
+import com.example.architectureapp.viewModel.NoteViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel mNoteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mNoteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+
+        this.mNoteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
