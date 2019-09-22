4aea43ca3ac01385e869af2f7d35acb120e57bcd
Gilles Loriquer <gilles.loriquer@gmail.com>
2019-09-22 17:22:07 +0200

    Implémentation du DataBinding

--- app/src/main/java/com/example/architectureapp/MainActivity.java
+++ app/src/main/java/com/example/architectureapp/MainActivity.java
@@ -1,31 +1,43 @@
 package com.example.architectureapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 import com.example.architectureapp.database.Note;
+import com.example.architectureapp.databinding.ActivityMainBinding;
 import com.example.architectureapp.viewModel.NoteViewModel;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     private NoteViewModel mNoteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        this.mNoteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        this.mNoteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+        // THIS IS EQUAL
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mNoteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        binding.setNoteViewModel(mNoteViewModel);
+        binding.setLifecycleOwner(this);
+
+        mNoteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                binding.textview.setText("Liste de notes : " + mNoteViewModel.getAllNotes().getValue().size());
             }
         });
     }
