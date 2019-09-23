86349faf297cecb5b0ccdea469e72b512f54b3da
gilles loriquer <gilles.loriquer@gfi.fr>
2019-09-23 15:45:18 +0200

    Implémentation du recyclerview

--- app/src/main/java/com/example/architectureapp/MainActivity.java
+++ app/src/main/java/com/example/architectureapp/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.architectureapp;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.architectureapp.adapter.NoteAdapter;
 import com.example.architectureapp.database.Note;
 import com.example.architectureapp.databinding.ActivityMainBinding;
 import com.example.architectureapp.viewModel.NoteViewModel;
@@ -23,21 +25,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
         // THIS IS EQUAL
         //binding = ActivityMainBinding.inflate(getLayoutInflater());
         //setContentView(binding.getRoot());
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mNoteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         binding.setNoteViewModel(mNoteViewModel);
         binding.setLifecycleOwner(this);
 
+        final NoteAdapter adapter = new NoteAdapter();
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+
         mNoteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
-                binding.textview.setText("Liste de notes : " + mNoteViewModel.getAllNotes().getValue().size());
+                adapter.setNotes(notes);
             }
         });
     }
---
+++ app/src/main/java/com/example/architectureapp/adapter/NoteAdapter.java
@@ -0,0 +1,63 @@
+package com.example.architectureapp.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.architectureapp.R;
+import com.example.architectureapp.database.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note note = this.notes.get(position);
+        holder.bind(note);
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewPriority;
+        private TextView textViewDescription;
+
+        NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+        }
+
+        void bind(Note note) {
+            textViewTitle.setText(note.getTitle());
+            textViewPriority.setText(String.valueOf(note.getPriority()));
+            textViewDescription.setText(note.getDescription());
+        }
+    }
+}
